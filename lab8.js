0e32b9e3ff3bb552aae9ff30c95a452fbb2128ac
Каменева Наталья <kameneva.natalka@gmail.com>
2023-06-06 15:01:00 +0700

    Вывод даты

--- lab8.js
+++ lab8.js
@@ -28,3 +28,12 @@ function showDate()   {
     outttttt.style.color="red"
     outttttt.style.fontSize="25px"
 }
+
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor (daysCount);
+    document.getElementById('dt').innerHTML = 'количество дней с даты рождения:' + daysCount;
+}
